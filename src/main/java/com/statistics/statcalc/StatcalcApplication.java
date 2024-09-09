4746a75c025f932ef9e7696b8b2bdd130d23d515
Joseph.W <joseph.w.edu@gmail.com>
2024-09-08 23:20:38 -0700

    Initial Commit
    Just throwing the basics up to the repo, will be throwing much more later on

---
+++ src/main/java/com/statistics/statcalc/StatcalcApplication.java
@@ -0,0 +1,13 @@
+package com.statistics.statcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatcalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatcalcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/statistics/statcalc/StatcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statistics.statcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatcalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
